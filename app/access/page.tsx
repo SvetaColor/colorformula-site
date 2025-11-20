c61371199908afdd851503f78132bd810bf61efd
SvetaColor <SvetaR81@mail.ru>
2025-11-20 22:52:20 +0300

    Update page.tsx

---
+++ app/access/page.tsx
@@ -0,0 +1,8 @@
+export default function AccessPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Страница доступа</h1>
+      <p>Здесь появится информация для клиентов с активной подпиской.</p>
+    </div>
+  );
+}
